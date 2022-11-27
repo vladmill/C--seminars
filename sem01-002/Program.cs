3a66727692c75de5fdcd5e56fbe93a2b8c31bc2e
vladmill <vladmill@mail.ru>
2022-11-27 09:44:37 +0700

    add

---
+++ sem01-002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int squareNum2 = num2*num2;
+if(squareNum2 == num1){
+	Console.WriteLine("Первое число является квадратом второго");
+}else{
+	Console.WriteLine("Первое число не является квадратом второго");
+}
