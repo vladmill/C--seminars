e734507692df32f5ce4275b0b40ef196fed31988
vladmill <vladmill@mail.ru>
2022-11-27 12:24:21 +0700

    dz-01 complete

---
+++ sem01-dz-04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 2){
+	Console.WriteLine("Попробуйте еще раз, с числом больше 1");
+}else{
+	// int count = 2;
+	// while(count <= num){
+	// 	Console.Write($"{count} ");
+	// 	count = count + 2;
+	// }
+	int count = 1;
+	while(count <= num){
+		if(count % 2 == 0){
+			Console.Write($"{count} ");
+		}
+		count++;
+	}
+}
---
+++ sem01-dz-05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число любой разрядности");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = -1;
+while(num > 99){
+	digit = num % 10;
+	num = num/10;
+}
+if( digit == -1){
+	Console.Write($"Введённое число меньше 100");
+}else{
+	Console.Write($"Третья цифра введённого числа {digit}");
+}
