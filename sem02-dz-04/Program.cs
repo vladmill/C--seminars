be9fe4f53f96cc66355e72e2835e60e72356361d
vladmill <vladmill@mail.ru>
2022-12-03 13:02:41 +0700

    add sem02

---
+++ sem02-dz-04/Program.cs
@@ -0,0 +1,11 @@
+﻿string checkMultiple(int num){
+	if(num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+	else if(num % 3 == 0) return "Fizz";
+	else if(num % 5 == 0) return "Buzz";
+	else return "none";
+}
+for(int i = 1; i<=100; i++) {
+	string result = checkMultiple(i);
+	if(result == "none") Console.Write($"{i} ");
+	else Console.Write($"{result} ");
+}
---
+++ sem02-dz-05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Сколько программистов в комнате?");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string declination(int num) {  
+	string[] forms = {"программист", "программиста", "программистов"};
+	int rem100 = num % 100;
+	int rem10 = num % 10;
+    if (rem100 > 10 && rem100 < 20) return forms[2];
+	else if (rem10 > 1 && rem10 < 5) return forms[1];
+    else if (rem10 == 1) return forms[0];
+    else return forms[2];
+}
+Console.Write($"В комнате {num} {declination(num)}");
